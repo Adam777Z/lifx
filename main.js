aa00debdf9e850b1668b0a4fc9781423685d8a1e
Adam777 <Adam777-5867@outlook.com>
2021-10-09 12:25:27 +0000

    Update

--- main.js
+++ main.js
@@ -34,10 +34,10 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(function () {
+app.whenReady().then(() => {
 	createWindow();
 
-	app.on('activate', function () {
+	app.on('activate', () => {
 		// On macOS it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
 		if (BrowserWindow.getAllWindows().length === 0) {
@@ -49,7 +49,7 @@ app.whenReady().then(function () {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
 		app.quit();
 	}
@@ -58,10 +58,10 @@ app.on('window-all-closed', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-ipcMain.on('quitApp', function () {
+ipcMain.on('quitApp', () => {
 	app.quit();
 });
 
-ipcMain.on('lock', function () {
+ipcMain.on('lock', () => {
 	require('child_process').exec('rundll32.exe user32.dll, LockWorkStation');
 });
